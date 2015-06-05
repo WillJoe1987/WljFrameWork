67c62c5684372606b286643216dd7cb82c847f7f
WillJoe1987 <littlefish_0130@163.com>
2015-06-05 09:20:53 +0800

    Something unused deleting.

--- Wlj/WebContent/contents/frameControllers/WljAPPBooter.js
+++ Wlj/WebContent/contents/frameControllers/WljAPPBooter.js
@@ -41,8 +41,6 @@
 	}
 	
 	scriptVersionControl("/contents/frameControllers/Wlj-search-APP-cfg.js");
-	scriptVersionControl("/contents/pages/common/Com.yucheng.crm.common.CustFlow.js");
-	scriptVersionControl("/contents/pages/common/Com.yucheng.crm.common.FusionChartPanel.js");
-	scriptVersionControl("/FusionCharts/FusionCharts.js");
-	scriptVersionControl("/contents/frameControllers/plugin/customerView/Wlj-frame-View.js");	
+//	scriptVersionControl("/contents/pages/common/Com.yucheng.crm.common.FusionChartPanel.js");
+//	scriptVersionControl("/FusionCharts/FusionCharts.js");
 })();
--- Wlj/WebContent/contents/frameControllers/WljFunctionBooter.js
+++ Wlj/WebContent/contents/frameControllers/WljFunctionBooter.js
@@ -9,48 +9,6 @@
 	 * runMod: 【debug:调试模式，run：运行模式，dev开发模式】
 	 */
 	var 	runMod = 'dev';
-//    var scripts = document.getElementsByTagName('script'),
-//        localhostTests = [
-//            /^localhost$/,
-//            /\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:\d{1,5})?\b/ // IP v4
-//        ],
-//        host = window.location.hostname,
-//        isDevelopment = null,
-//        queryString = window.location.search,
-//        test, path, i, ln, scriptSrc, match;
-//
-//    for (i = 0, ln = scripts.length; i < ln; i++) {
-//        scriptSrc = scripts[i].src;
-//
-//        match = scriptSrc.match(/bootstrap\.js$/);
-//
-//        if (match) {
-//            path = scriptSrc.substring(0, scriptSrc.length - match[0].length);
-//            break;
-//        }
-//    }
-//
-//    if (queryString.match('(\\?|&)debug') !== null) {
-//        isDevelopment = true;
-//    }
-//    else if (queryString.match('(\\?|&)nodebug') !== null) {
-//        isDevelopment = false;
-//    }
-//
-//    if (isDevelopment === null) {
-//        for (i = 0, ln = localhostTests.length; i < ln; i++) {
-//            test = localhostTests[i];
-//
-//            if (host.search(test) !== -1) {
-//                isDevelopment = true;
-//                break;
-//            }
-//        }
-//    }
-//
-//    if (isDevelopment === null && window.location.protocol === 'file:') {
-//        isDevelopment = true;
-//    }
 	
 	linkVersionControl("/contents/wljFrontFrame/styles/search/searchcss/common.css");
 	linkVersionControl("/contents/wljFrontFrame/styles/search/searchcss/base_frame.css");
@@ -81,17 +39,12 @@
 	    scriptVersionControl("/contents/frameControllers/widgets/app/Wlj-frame-function-app.js");
 	    scriptVersionControl("/contents/frameControllers/widgets/app/Wlj-frame-function-builder.js");
 	    scriptVersionControl("/contents/frameControllers/widgets/app/plugins/thumbnails/Wlj-frame-functions-plugin-thumbnails.js");
-	    
 	}else if(runMod=='debug'){
 		scriptVersionControl("/contents/frameControllers/Wlj.function.all-v1.0.x-debug.js");
 	}else{
 		scriptVersionControl("/contents/frameControllers/Wlj.function.all-v1.0.x-min.js"); 
 	}
-	
-	
-	
     scriptVersionControl("/contents/frameControllers/widgets/app/Wlj-frame-function-header.js");
     scriptVersionControl("/contents/frameControllers/widgets/app/Wlj-frame-function-api.js");
     scriptVersionControl("/contents/frameControllers/systemPatch.js");
-    
 })();
